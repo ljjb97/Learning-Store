712415310b88de3740d72e1fe0d27ae9d79918fc
Liam Ballantyne <ljjb97@gmail.com>
2018-10-23 00:57:10 -0400

    Fixed typos preventing access to schema and env

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,10 @@
-//This file connects to the remotre prisma db and gives up the
+//This file connects to the remote prisma db and gives up the
 //abilityh to query it in js
 
 const { Prisma } = require('prisma-binding');
+
+console.log(process.env.PRISMA_ENDPOINT);
+console.log(`${process.env.PORT}`)
 
 const db = new Prisma({
     typeDefs: 'src/generated/prisma.graphql',
